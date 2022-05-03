a2ad40b37fce46c2e0b1dd2d5c051556cf74ecd7
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-05-03 10:22:11 -0400

    added auth

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# hydrants.net-2022
+# emergencyServices.net-2022
 run with:
 npm start
 go to:
 http://localhost:3000
-
-don't forget we are only showing 1/10 hydrants so the run's programm much faster
-
 require node_modules (npm i)
--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios').default;
+const auth = require('../Middleware/auth');
+
+router.use(auth);
+
+router.get('/', function (req, res, next) {
+    if (req.auth.isAuthenticated) {
+        res.json({ result: true });
+
+    } else {
+        res.json({ result: false });
+    }
+});
 
 
-router.get('/hydrants', async function(req, res) {
 
-    let url = "https://www.gatineau.ca/upload/donneesouvertes/BORNE_FONTAINE.xml";
-    // let url = "https://maps.ottawa.ca/arcgis/rest/services/Hospitals/MapServer/0/query?outFields=*&where=1%3D1&f=geojson";
+router.get('/servicesOttawa', async function (req, res) {
+
+    let url = "http://maps.ottawa.ca/arcgis/rest/services/City_Facilities/MapServer/3/query?outFields=*&where=1%3D1&f=geojson";
     let aResponse = await axios.get(url);
 
-   
-    res.header("Content-Type", "application/xml");
-    // res.header("Content-Type", "application/json");
-    res.send(aResponse.data);
+    res.json(aResponse.data);
+});
 
+router.get('/servicesGatineau', async function (req, res) {
+
+    let url = "http://www.gatineau.ca/upload/donneesouvertes/LIEU_PUBLIC.json";
+    let aResponse = await axios.get(url);
+
+    res.json(aResponse.data);
 });
 
 module.exports = router;
